661c4fb959067225cc9101c8fca7eab61c05e0f6
Puneet Kumar <puneetk49081@gmail.com>
2024-07-25 12:57:00 +0000

    connected to mongo atla

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app=express()
+// using IIFE using function imediately
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",()=>{
+            console.log("ERR",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR",error);
+        throw err
+    }
+})()      */
